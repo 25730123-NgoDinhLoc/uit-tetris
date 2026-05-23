9cb41a7a895ac8e82609e516b9f7c630df7f698b
25730106PhamDoNgocDiep <25730106@ms.uit.edu.vn>
2026-05-24 00:11:21 +0700

    Add files via upload

--- tetris.cpp
+++ tetris.cpp
@@ -14,6 +14,9 @@ Tetris::Tetris()
     srand(static_cast<unsigned int>(time(nullptr)));
     initializePieces();
     resetBoard();
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+    nextPieceIndex = std::rand() % NUM_PIECES;
+    spawnNewPiece();
     (void)boardScreenRow;
     (void)boardScreenCol;
 }
@@ -54,58 +57,126 @@ void Tetris::initializePieces() {
 // Sinh khối mới
 // ---------------------------------------------------------------------------
 
-void Tetris::spawnNewPiece() {}
+void Tetris::spawnNewPiece() {
+    falling.pieceIndex = nextPieceIndex;
+    nextPieceIndex     = std::rand() % NUM_PIECES;
+    falling.rotation   = 0;
+    falling.posX       = GameBoard::WIDTH / 2 - 2; // Căn giữa hộp bao 4 ô
+    falling.posY       = 0;
+    hasHeldThisTurn    = false;
+
+    if (checkCollision(falling.pieceIndex, falling.rotation, falling.posX, falling.posY)) {
+        status.isGameOver = true;
+    }
+}
 
 // ---------------------------------------------------------------------------
 // Va chạm
 // ---------------------------------------------------------------------------
 
 bool Tetris::checkCollision(int pieceIndex, int rotation, int posX, int posY) const {
-    (void)pieceIndex;
-    (void)rotation;
-    (void)posX;
-    (void)posY;
-    return true;
+    const RotationGrid& grid = pieceDefinitions[pieceIndex].allRotations[rotation];
+    for (int row = 0; row < GRID_SIZE; ++row) {
+        for (int col = 0; col < GRID_SIZE; ++col) {
+            if (grid.cells[row][col] == 0) continue; // Ô trống trong hộp bao
+            int boardCol = posX + col;
+            int boardRow = posY + row;
+            if (boardCol < 0 || boardCol >= GameBoard::WIDTH) return true;  // Va tường ngang
+            if (boardRow >= GameBoard::HEIGHT)          return true;  // Va sàn
+            if (boardRow < 0)                           continue;     // Trên trần là bình thường
+            if (board.cells[boardRow][boardCol] != 0)   return true;  // Va khối đã khóa
+        }
+    }
+    return false;
 }
 
 // ---------------------------------------------------------------------------
 // Khóa khối vào bảng
 // ---------------------------------------------------------------------------
 
-void Tetris::lockPieceToBoard() {}
+void Tetris::lockPieceToBoard() {
+    const RotationGrid& grid = pieceDefinitions[falling.pieceIndex].allRotations[falling.rotation];
+    for (int row = 0; row < GRID_SIZE; ++row) {
+        for (int col = 0; col < GRID_SIZE; ++col) {
+            if (grid.cells[row][col] == 0) continue;
+            int boardCol = falling.posX + col;
+            int boardRow = falling.posY + row;
+            if (boardRow >= 0 && boardRow < GameBoard::HEIGHT && boardCol >= 0 && boardCol < GameBoard::WIDTH) {
+                board.cells[boardRow][boardCol] = grid.cells[row][col];
+            }
+        }
+    }
+}
+
 
 // ---------------------------------------------------------------------------
 // Xóa hàng đầy
 // ---------------------------------------------------------------------------
 
 bool Tetris::isRowFull(int row) const {
-    (void)row;
-    return false;
+    for (int col = 0; col < GameBoard::WIDTH; ++col)
+        if (board.cells[row][col] == 0) return false;
+    return true;
 }
 
 void Tetris::deleteRowAndShiftDown(int deletedRow) {
-    (void)deletedRow;
+    for (int row = deletedRow; row > 0; --row)
+        for (int col = 0; col < GameBoard::WIDTH; ++col)
+            board.cells[row][col] = board.cells[row - 1][col];
+
+    for (int col = 0; col < GameBoard::WIDTH; ++col)
+        board.cells[0][col] = 0;
 }
 
+
 int Tetris::clearFullLines() {
-    return 0;
+    int linesCleared = 0;
+    for (int row = GameBoard::HEIGHT - 1; row >= 0; --row) {
+        if (isRowFull(row)) {
+            deleteRowAndShiftDown(row);
+            linesCleared++;
+            row++; // Kiểm tra lại hàng đã xóa
+        }
+    }
+    return linesCleared;
 }
 
 // ---------------------------------------------------------------------------
 // Xoay khối
 // ---------------------------------------------------------------------------
 
-void Tetris::rotatePiece() {}
+void Tetris::rotatePiece() {
+     int nextRotation = (falling.rotation + 1) % NUM_ROTATIONS;
+    // Thử các độ dịch chuyển ngang: 0, -1, +1, -2, +2 ô.
+    int horizontalShifts[] = {0, -1, 1, -2, 2};
+    for (int shift : horizontalShifts) {
+        if (!checkCollision(falling.pieceIndex, nextRotation, falling.posX + shift, falling.posY)) {
+            falling.rotation = nextRotation;
+            falling.posX += shift;
+            return;
+        }
+    }
+}
 
 // ---------------------------------------------------------------------------
 // Di chuyển
 // ---------------------------------------------------------------------------
 
-void Tetris::movePieceLeft() {}
+void Tetris::movePieceLeft() {
+    if (!checkCollision(falling.pieceIndex, falling.rotation, falling.posX - 1, falling.posY))
+        --falling.posX;
+}
 
-void Tetris::movePieceRight() {}
+void Tetris::movePieceRight() {
+    if (!checkCollision(falling.pieceIndex, falling.rotation, falling.posX + 1, falling.posY))
+        ++falling.posX;
+}
 
 bool Tetris::tryMoveDownOneCell() {
+    if (!checkCollision(falling.pieceIndex, falling.rotation, falling.posX, falling.posY + 1)) {
+        ++falling.posY;
+        return true;
+    }
     return false;
 }
 
@@ -115,32 +186,72 @@ bool Tetris::tryMoveDownOneCell() {
 
 // TODO: implement ghost calculation
 int Tetris::calculateGhostPositionY() const {
-    return 0;
+   int ghostPosY = falling.posY;
+    while (!checkCollision(falling.pieceIndex, falling.rotation, falling.posX, ghostPosY + 1))
+        ++ghostPosY;
+    return ghostPosY;
 }
 
+
 int Tetris::calculateScoreForLines(int numLines) const {
-    (void)numLines;
-    return 0;
+    switch (numLines) {
+        case 1: return 100;
+        case 2: return 300;
+        case 3: return 500;
+        case 4: return 800;
+        default: return 0;
+    }
 }
 
 // ---------------------------------------------------------------------------
 // Hold piece
 // ---------------------------------------------------------------------------
 
-void Tetris::holdCurrentPiece() {}
+void Tetris::holdCurrentPiece() {
+    if (hasHeldThisTurn || status.isGameOver) {
+        return;
+    }
+
+    if (holdPieceIndex == -1) {
+        holdPieceIndex = falling.pieceIndex;
+        spawnNewPiece();
+    } else {
+        int temp = falling.pieceIndex;
+        falling.pieceIndex = holdPieceIndex;
+        holdPieceIndex = temp;
+        falling.posX = GameBoard::WIDTH / 2 - 2;
+        falling.posY = 0;
+        falling.rotation = 0;
+        if (checkCollision(falling.pieceIndex, falling.rotation, falling.posX, falling.posY)) {
+            status.isGameOver = true;
+        }
+    }
+    hasHeldThisTurn = true;
+
+}
 
 // ---------------------------------------------------------------------------
 // Lock piece + spawn next
 // ---------------------------------------------------------------------------
 
-void Tetris::lockPieceAndSpawnNext() {}
+void Tetris::lockPieceAndSpawnNext() {
+     lockPieceToBoard();
+    int linesJustCleared = clearFullLines();
+    if (linesJustCleared > 0) {
+        status.totalLinesCleared += linesJustCleared;
+        status.score += calculateScoreForLines(linesJustCleared) * status.level;
+        status.level = 1 + status.totalLinesCleared / 10;
+    }
+    spawnNewPiece();
+}
 
 // ---------------------------------------------------------------------------
 // Thờigian
 // ---------------------------------------------------------------------------
 
 int Tetris::calculateGravityDelayMs() const {
-    return 600;
+    int delay = 600 - (status.level - 1) * 50;
+    return (delay < 50) ? 50 : delay;
 }
 
 // ---------------------------------------------------------------------------
@@ -216,7 +327,12 @@ void Tetris::drawPauseOverlay() {}
 // ---------------------------------------------------------------------------
 
 int Tetris::dropToBottom() {
-    return 0;
+    int droppedCells = 0;
+    while (!checkCollision(falling.pieceIndex, falling.rotation, falling.posX, falling.posY + 1)) {
+        ++falling.posY;
+        ++droppedCells;
+    }
+    return droppedCells;
 }
 
 // ---------------------------------------------------------------------------
